c47efe2ab492d585fa77e992ad60694c7a1c055a
Villegunnar <villegunnar@gmail.com>
2022-06-13 20:35:17 +0200

    Add project files.

---
+++ CalculatorLabb3/Calculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorLabb3
+{
+    public class Calculator
+    {
+        public double input1;
+        public double input2;
+        public static List<string> CalculationHistory;
+
+        public Calculator()
+        {
+            CalculationHistory = new List<string>();
+        }
+
+        public double Addition(double input1, double input2)
+        {
+            double result = input1 + input2;
+            string resultstring = $"{input1} + {input2} = {result}";
+            AddCalculationToList(resultstring);
+            return result;
+
+        }
+        public double Subtraction(double input1, double input2)
+        {
+            double result = input1 - input2;
+            string resultstring = $"{input1} - {input2} = {result}";
+            AddCalculationToList(resultstring);
+            return result;
+        }
+        public double Multiplication(double input1, double input2)
+        {
+            double result = input1 * input2;
+            string resultstring = $"{input1} * {input2} = {result}";
+            AddCalculationToList(resultstring);
+
+            return result;
+        }
+
+        public static void printCalc(string outPut)
+        {
+           
+            Console.WriteLine(outPut);
+        }
+        public void AddCalculationToList(string calc)
+        {
+            CalculationHistory.Add(calc);
+        }
+        public static void PrintCalcHistory()
+        {
+            Console.Clear();
+            if (CalculationHistory != null)
+            {
+                foreach (string item in CalculationHistory)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No results was found");
+            }
+ 
+        }
+    }
+}
---
+++ CalculatorLabb3/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace CalculatorLabb3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            while (true)
+            {
+                Console.WriteLine("1. Calculate \n2. Calculate History");
+                string menyChocie = Console.ReadLine();
+                switch (menyChocie)
+                {
+                    case "1":
+                        Console.Clear();
+                        Calculator calc = new Calculator();
+
+
+                        Console.WriteLine("Please enter a number");
+                        while (!double.TryParse(Console.ReadLine(), out calc.input1))
+                        {
+                            Console.WriteLine("Error, enter a number instead");
+                        }
+
+
+                        Console.WriteLine("Please enter another number");
+                        while (!double.TryParse(Console.ReadLine(), out calc.input2))
+                        {
+                            Console.WriteLine("Error, enter a number instead");
+                        }
+
+                        Console.WriteLine("Choose your from of calculation\n1. Addition\n2. Subtraction\n3. Multiplication");
+                        string calcForm = Console.ReadLine();
+                        switch (calcForm)
+                        {
+                            case "1":
+                                double additionResult = calc.Addition(calc.input1, calc.input2);
+                                Calculator.printCalc(Convert.ToString(additionResult));
+                                break;
+                            case "2":
+                                double subtractionResult = calc.Subtraction(calc.input1, calc.input2);
+                                Calculator.printCalc(Convert.ToString(subtractionResult));
+
+                                break;
+                            case "3":
+                                double multiplicationResult = calc.Multiplication(calc.input1, calc.input2);
+                                Calculator.printCalc(Convert.ToString(multiplicationResult));
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        break;
+                    case "2":
+                        Calculator.PrintCalcHistory();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
